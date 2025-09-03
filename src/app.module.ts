6ca60fb627c66d90ba2810bf7cec8786b7883449
Hritik-rr <hritikranjan17@gmail.com>
2025-09-03 20:16:54 +0530

    db setup completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entity/user.entity';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER || 'admin',
+      password: process.env.DB_PASS || 'admin',
+      database: process.env.DB_NAME || 'coin_tab_db',
+      synchronize: true,
+      entities: [User],
+      logging: false,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
